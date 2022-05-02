dbea4bedfbd8eaf07cc1545de5b8d994d6ef008f
Craig Thacker <craigthackerx@gmail.com>
2022-05-02 15:48:53 +0100

    Update module

--- input.tf
+++ input.tf
@@ -1,10 +1,10 @@
 variable "standard_os" {
   default = {
-    "CISWindowsServer2012L2"          = "center-for-internet-security-inc,cis-windows-server-2012-r2-v2-2-1-l2,cis-ws2012-r2-l2",
-    "CISWindowsServer2016L1"          = "center-for-internet-security-inc,cis-windows-server-2016-v1-0-0-l1,cis-ws2016-l1",
-    "CISWindowsServer2016L2"          = "center-for-internet-security-inc,cis-windows-server-2016-v1-0-0-l2,cis-ws2016-l2",
-    "CISWindowsServer2019L1"          = "center-for-internet-security-inc,cis-windows-server-2019-v1-0-0-l1,cis-ws2019-l1",
-    "CISWindowsServer2019L2"          = "center-for-internet-security-inc,cis-windows-server-2019-v1-0-0-l2,cis-ws2019-l2",
+    "CISWindowsServer2012L2" = "center-for-internet-security-inc,cis-windows-server-2012-r2-v2-2-1-l2,cis-ws2012-r2-l2",
+    "CISWindowsServer2016L1" = "center-for-internet-security-inc,cis-windows-server-2016-v1-0-0-l1,cis-ws2016-l1",
+    "CISWindowsServer2016L2" = "center-for-internet-security-inc,cis-windows-server-2016-v1-0-0-l2,cis-ws2016-l2",
+    "CISWindowsServer2019L1" = "center-for-internet-security-inc,cis-windows-server-2019-v1-0-0-l1,cis-ws2019-l1",
+    "CISWindowsServer2019L2" = "center-for-internet-security-inc,cis-windows-server-2019-v1-0-0-l2,cis-ws2019-l2",
   }
 }
 
